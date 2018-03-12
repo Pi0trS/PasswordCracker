3715d02fc0f621009f1270587c37c0dc26703a33
Pi0trS <piotrsy82@gmail.com>
2018-03-13 00:07:48 +0100

    add class UserData

---
+++ PasswordCracker/UserData.cpp
@@ -0,0 +1,5 @@
+#include "UserData.h"
+#include<string>
+UserData::UserData()
+{
+}
---
+++ PasswordCracker/UserData.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class UserData
+{
+public:
+	UserData();
+	void hashSet(std::string hash_);
+	void hashTypeSet(std::string hsahType_);
+	void saltSet(std::string salt_);
+	void dictionaryPathSet(std::string path);
+	void numberOfThredeSet(std::string number);
+
+private:
+	std::string hash, hashType, salt, dictionaryPath, numberOfThread;
+ 
+};
